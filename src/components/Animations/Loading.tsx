bf7807b8e8f5754baa0a4901c47d980e88de50fe
IBR4NX <ibrahim.al.mekhlafi.my@gmail.com>
2026-01-26 00:24:47 +0300

    first commit

---
+++ src/components/Animations/Loading.tsx
@@ -0,0 +1,7 @@
+import { AiOutlineLoading } from "react-icons/ai";
+
+export default function Loading(size:24) {
+  return (
+      <AiOutlineLoading size={size} className="animate-spin  text-gold" />
+  );
+}
